ee1fdc24dceb485567b820380d041384967e203f
ImCodist <ImCodist@users.noreply.github.com>
2025-09-03 04:42:01 +0000

    deploy: d6f56825ce787e2a8e0407b6be54ad09c59b0254

--- js/base.js
+++ js/base.js
@@ -1,12 +1,12 @@
 $(function() {
     $(".squeakable").click(function() {
+        var audio = new Audio("/assets/audio/squeaky/squeak-" + (Math.floor(Math.random() * 7) + 1) + ".ogg");
+        audio.play();
+        
         var squeaker = $(this);
         squeaker.removeClass("run");
         setTimeout(function() {
-            var audio = new Audio("/assets/audio/squeaky/squeak-" + (Math.floor(Math.random() * 7) + 1) + ".ogg");
-            audio.play();
-        
             squeaker.addClass("run");
-        }, 1)
+        }, 1);
     })
 })
